8c382b95c26a5adcf2c56557f53487284216fa57
Paul Brady <pablo.brady@gmail.com>
2019-09-18 18:59:37 -0700

    Success

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-## XXXXXX - JS Toy Problem
+## Making Change - JS Toy Problem
 
-**XXXXXX Problem Description:**
+Write a function given:
+an amount of money
+an array of coin denominations
+computes the number of ways to make the amount of money with coins of the available denominations.
 
+Example: for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 33¢), your program would output 44—the number of ways to make 44¢ with those denominations:
 
+1¢, 1¢, 1¢, 1¢
+1¢, 1¢, 2¢
+1¢, 3¢
+2¢, 2¢
 
 ##
 
---
-### CODE TESTING:
-
-```npm test```
-
-or
-
-```mocha ./node_modules/mocha/bin/mocha```
-
-
-
----
-#### REFERENCE:
-- https://www.sitepoint.com/unit-test-javascript-mocha-chai/
-
-- https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/
-
-- https://mochajs.org/#getting-started
+Skipping Mocha Test on this example
--- solved.js
+++ solved.js
@@ -1,32 +1,94 @@
-/* ./index.js */
+class CoinWrapper {
 
-module.exports = function() {
-
-  // Convert arguments object to an array
-  var args = Array.prototype.slice.call(arguments);
-
-  // Arguments should contain (finite) number values
-  // if (!args.every(Number.isFinite)) {
-  //   throw new TypeError('Expecting only numbers.')
-  // }
-
-  // Debugging assistance
-  var sleep = function (milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
+  constructor() {
+    this.store = {}
+  }
+  
+  changePossibilities(amountLeft, denominations, currentCoinIndex = 0) {
+    // Calculate the number of ways to make change
+    // denominations --- The types of coins allowed in this test
+    
+    const storageKey = [amountLeft, currentCoinIndex].join(', ')
+    if (this.store.hasOwnProperty(storageKey)) {
+      // console.log('grabbing from "store" [' + storageKey + ']...  ')
+      return this.store[storageKey]; // 0, aka. a cached Success!
     }
+
+  
+    if (amountLeft === 0) return 1; // Success
+  
+    if (amountLeft < 0 ) return 0;  // Overshot
+  
+    if (currentCoinIndex === denominations.length) return 0;  // No more coin types to try
+
+
+  
+    const currentCoin = denominations[currentCoinIndex]
+  
+    // See how many possibilities we can get for type of coin
+    let numPossibilities = 0;
+    while ( amountLeft >= 0 ) {
+
+      numPossibilities += this.changePossibilities(amountLeft, denominations, currentCoinIndex + 1);
+      amountLeft -= currentCoin;
+
+    }
+
+    this.store[storageKey] = numPossibilities;
+    return numPossibilities;
   }
 
-  // Use the "solution(xx)" function to return data for testing.
-  var solution = function (input) {
-    const MAX_IDX = input;
-    var outputArray = [-1, input]; 
-
-    return outputArray
-  }
-
-  return solution(args[0]);
 }
+
+
+// Tests
+
+let desc = 'sample input';
+let coinWrapper = new CoinWrapper();
+let actual = coinWrapper.changePossibilities(4, [1, 2, 3]);
+let expected = 4;  // WAYS TO SOLVE, USING COINS 1, 2, & 3 cent pieces
+assertEqual(actual, expected, desc);
+
+desc = 'one way to make zero cents';
+coinWrapper = new CoinWrapper();
+actual = coinWrapper.changePossibilities(0, [1, 2]);
+expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'no ways if no coins';
+coinWrapper = new CoinWrapper();
+actual = coinWrapper.changePossibilities(1, []);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big coin value';
+coinWrapper = new CoinWrapper();
+actual = coinWrapper.changePossibilities(5, [25, 50]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'big target amount';
+coinWrapper = new CoinWrapper();
+actual = coinWrapper.changePossibilities(50, [5, 10]);
+expected = 6;
+assertEqual(actual, expected, desc);
+
+desc = 'change for one dollar';
+coinWrapper = new CoinWrapper();
+actual = coinWrapper.changePossibilities(100, [1, 5, 10, 25, 50]);
+expected = 292;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  let el = document.getElementById("output");
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+    el.innerHTML = el.innerHTML + '<br><br>' + desc + ' ... <span style="color:green"> PASS </span>';
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+    el.innerHTML = el.innerHTML + '<br><br>' + desc + ' ... <span style="color:#ff0033"> FAIL </span>';
+  }
+}
+
+
+
